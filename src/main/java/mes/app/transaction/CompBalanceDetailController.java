442eb41383af96c92d6975fa4ade6a41b3b04dbb
DESKTOP-TG6HNLE\Act-PC-11_yuri <kyl1563@naver.com>
2025-05-12 19:01:21 +0900

    2025.05.12 - 김유리 - 거래처별 잔액 명세(입금) 수정

--- src/main/java/mes/app/transaction/CompBalanceDetailController.java
+++ src/main/java/mes/app/transaction/CompBalanceDetailController.java
@@ -25,16 +25,12 @@ public class CompBalanceDetailController {
   // 거래처잔액 명세(입금 관리)
   @GetMapping("/read")
   public AjaxResult getList(
-      @RequestParam(value="srchStartDt", required=false) String start_date,
-      @RequestParam(value="srchEndDt", required=false) String end_date,
+      @RequestParam(value="srchStartDt", required=false) String start,
+      @RequestParam(value="srchEndDt", required=false) String end,
       @RequestParam(value = "cboCompany", required=false) String company,
       @RequestParam(value = "spjangcd") String spjangcd,
       HttpServletRequest request) {
     //log.info("거래처잔액 명세(입금 관리) read ---  :start:{}, end:{} ,company:{}, spjangcd:{} ", start_date, end_date, company, spjangcd);
-    start_date = start_date + " 00:00:00";
-    end_date = end_date + " 23:59:59";
-    Timestamp start = Timestamp.valueOf(start_date);
-    Timestamp end = Timestamp.valueOf(end_date);
 
     List<Map<String, Object>> items = this.compBalanceDetailServicr.getList(start, end, company,spjangcd);
 
--- src/main/java/mes/app/transaction/service/CompBalanceDetailServicr.java
+++ src/main/java/mes/app/transaction/service/CompBalanceDetailServicr.java
@@ -1,13 +1,14 @@
 package mes.app.transaction.service;
 
 import lombok.extern.slf4j.Slf4j;
+import mes.Encryption.EncryptionUtil;
 import mes.domain.services.SqlRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
@@ -20,7 +21,7 @@ public class CompBalanceDetailServicr {
   @Autowired
   SqlRunner sqlRunner;
 
-  public List<Map<String, Object>> getList(Timestamp start, Timestamp end, String company, String spjangcd) {
+  public List<Map<String, Object>> getList(String start, String end, String company, String spjangcd) {
 
     if (company == null || company.trim().isEmpty()) {
       //log.warn("⚠️ 거래처 코드가 비어 있습니다. 빈 리스트를 반환합니다.");
@@ -28,63 +29,71 @@ public class CompBalanceDetailServicr {
     }
 
     MapSqlParameterSource paramMap = new MapSqlParameterSource();
-    paramMap.addValue("start", start);
-    paramMap.addValue("end", end);
     paramMap.addValue("company", Integer.parseInt(company));
     paramMap.addValue("spjangcd", spjangcd);
+    DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    DateTimeFormatter dbFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
+    LocalDate startDate = LocalDate.parse(start, inputFormatter);
+    LocalDate endDate = LocalDate.parse(end, inputFormatter);
 
-    // Timestamp → LocalDate
-    LocalDate startDate = start.toLocalDateTime().toLocalDate();
+    // SQL용 날짜 포맷
+    String formattedStart = startDate.format(dbFormatter);
+    String formattedEnd = endDate.format(dbFormatter);
 
-    // 전월 구하기
-    LocalDate prevMonth = startDate.minusMonths(1);
-
-    // yyyymm
+    // 전월 기준 계산
+    YearMonth prevMonth = YearMonth.from(startDate).minusMonths(1);
     String prevYm = prevMonth.format(DateTimeFormatter.ofPattern("yyyyMM"));
+    String baseDate = prevMonth.atDay(1).format(dbFormatter);
 
-    // 기준일
-    String baseDate = prevMonth.withDayOfMonth(1).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-
-    // SQL 파라미터 등록
+    // 파라미터 등록
+    paramMap.addValue("start", formattedStart);
+    paramMap.addValue("end", formattedEnd);
     paramMap.addValue("prevYm", prevYm);
     paramMap.addValue("baseDate", baseDate);
 
 
     String sql = """
-        WITH lasttbl AS (
-            SELECT cltcd, yearamt, MAX(yyyymm) AS yyyymm
+        WITH lastym AS (
+            SELECT cltcd, MAX(yyyymm) AS yyyymm
             FROM tb_yearamt
             WHERE yyyymm < :prevYm
               AND ioflag = '0'
               AND cltcd = :company
               AND spjangcd = :spjangcd
-            GROUP BY cltcd, yearamt
+            GROUP BY cltcd
+        ),
+        lasttbl AS (
+            SELECT y.cltcd, y.yearamt, y.yyyymm, y.spjangcd
+            FROM tb_yearamt y
+            JOIN lastym m ON y.cltcd = m.cltcd AND y.yyyymm = m.yyyymm
+            WHERE y.ioflag = '0'
+              AND y.spjangcd = :spjangcd
         ),
         saletbl AS (
-            SELECT s.cltcd, SUM(s.totalamt) AS totsale
+            SELECT s.cltcd, SUM(s.totalamt) AS totsale, s.spjangcd
             FROM tb_salesment s
             WHERE s.misdate BETWEEN :start AND :end
               AND s.cltcd = :company
               AND s.spjangcd = :spjangcd
-            GROUP BY s.cltcd
+            GROUP BY s.cltcd, s.spjangcd
         ),
         incomtbl AS (
-            SELECT s.cltcd, SUM(s.accout) AS totaccout
+            SELECT s.cltcd, SUM(s.accin) AS totaccout, s.spjangcd
             FROM tb_banktransit s
             WHERE s.trdate BETWEEN :start AND :end
               AND s.cltcd = :company
               AND s.spjangcd = :spjangcd
-            GROUP BY s.cltcd
+            GROUP BY s.cltcd, s.spjangcd
         ),
         union_data_raw AS (
             -- 전잔액
             SELECT
-                y.id,
-                y."Name" AS comp_name,
+                c.id,
+                c."Name" AS comp_name,
                 TO_DATE(:baseDate, 'YYYYMMDD') AS date,
                 '전잔액' AS summary,
-                SUM(h.yearamt) + SUM(P.totsale) - SUM(Q.totaccout) AS amount,
+                COALESCE(h.yearamt, 0) + COALESCE(p.totsale, 0) - COALESCE(q.totaccout, 0) AS amount,
                 NULL::text AS itemnm,
                 NULL::text AS misgubun,
                 NULL::text AS iotype,
@@ -93,16 +102,16 @@ public class CompBalanceDetailServicr {
                 NULL::text AS eumnum,
                 NULL::text AS eumtodt,
                 NULL::text AS tradenm,
-                NULL::numeric AS accin,
-                NULL::numeric AS accout,
+                NULL::numeric AS accin,                
+                NULL::numeric AS totalamt,
                 NULL::text AS memo,
                 NULL::text AS remark1,
                 0 AS remaksseq
-            FROM company y
-            JOIN lasttbl h ON y.id = h.cltcd
-            JOIN saletbl P ON y.id = P.cltcd
-            JOIN incomtbl Q ON y.id = Q.cltcd
-            GROUP BY y.id, y."Name"
+            FROM company c
+            LEFT JOIN lasttbl h ON c.id = h.cltcd AND c.spjangcd = h.spjangcd
+            LEFT JOIN saletbl p ON c.id = p.cltcd AND c.spjangcd = p.spjangcd
+            LEFT JOIN incomtbl q ON c.id = q.cltcd AND c.spjangcd = q.spjangcd
+            WHERE c.id = :company AND c.spjangcd = :spjangcd
             UNION ALL
             -- 매출
             SELECT
@@ -110,30 +119,31 @@ public class CompBalanceDetailServicr {
                 c."Name" AS comp_name,
                 TO_DATE(s.misdate, 'YYYYMMDD'),
                 '매출',
-                s.totalamt,
+                NULL::numeric AS amount,
                 CONCAT(
                     MAX(CASE WHEN d.misseq::int = 1 THEN d.itemnm END),
                     CASE WHEN COUNT(DISTINCT d.itemnm) > 1 THEN ' 외 ' || (COUNT(DISTINCT d.itemnm) - 1) || '건' ELSE '' END
                 ) AS itemnm,
-                s.misgubun,
+                sc."Value" AS misgubun,
                 NULL::text AS iotype,
                 NULL::text AS banknm,
                 NULL::text AS accnum,
                 NULL::text AS eumnum,
                 NULL::text AS eumtodt,
                 NULL::text AS tradenm,
-                NULL::numeric AS accin,
-                NULL::numeric AS accout,
+                NULL::numeric AS accin,               
+                s.totalamt AS totalamt,
                 NULL::text AS memo,
-                NULL::text AS remark1,
+                s.remark1,
                 1 AS remaksseq
             FROM tb_salesment s
             LEFT JOIN tb_salesdetail d ON s.misdate = d.misdate AND s.misnum = d.misnum AND s.spjangcd = d.spjangcd
+            LEFT JOIN sys_code sc ON sc."Code" = s.misgubun
             JOIN company c ON c.id = s.cltcd AND c.spjangcd = s.spjangcd
             WHERE s.misdate BETWEEN :start AND :end
               AND s.spjangcd = :spjangcd
               AND s.cltcd = :company
-            GROUP BY s.cltcd, c."Name", s.misdate, s.totalamt, s.misgubun
+            GROUP BY s.cltcd, c."Name", s.misdate, s.totalamt, s.misgubun, sc."Value", s.remark1
             UNION ALL
             -- 입금
             SELECT
@@ -148,10 +158,10 @@ public class CompBalanceDetailServicr {
                 b.banknm,
                 b.accnum,
                 b.eumnum,
-                TO_CHAR(TO_DATE(NULLIF(b.eumtodt, ''), 'YYYYMMDD'), 'YYYY-MM-DD') AS eumtodt,
+                TO_CHAR(TO_DATE(NULLIF(b.eumtodt, ''), 'YYYYMMDD'), 'YYYY-MM-DD'),
                 tt.tradenm,
                 b.accin,
-                b.accout,
+                NULL::numeric AS totalamt,
                 b.memo,
                 b.remark1,
                 2 AS remaksseq
@@ -159,9 +169,10 @@ public class CompBalanceDetailServicr {
             JOIN company c ON c.id = b.cltcd AND c.spjangcd = b.spjangcd
             LEFT JOIN sys_code sc ON sc."Code" = b.iotype
             LEFT JOIN tb_trade tt ON tt.trid = b.trid AND tt.spjangcd = b.spjangcd
-            WHERE TO_DATE(b.trdate, 'YYYYMMDD') BETWEEN :start AND :end
+             WHERE TO_DATE(b.trdate, 'YYYYMMDD') BETWEEN TO_DATE(:start, 'YYYYMMDD') AND TO_DATE(:end, 'YYYYMMDD') 
               AND b.cltcd = :company
               AND b.spjangcd = :spjangcd
+              and b.ioflag = '0'
         ),
         union_data AS (
             SELECT *,
@@ -174,6 +185,15 @@ public class CompBalanceDetailServicr {
             x.date,
             x.summary,
             x.amount,
+            x.accin,
+            x.totalamt,
+           SUM(
+            COALESCE(x.amount, 0) + COALESCE(x.totalamt, 0) - COALESCE(x.accin, 0)
+        ) OVER (
+            PARTITION BY x.id
+            ORDER BY x.date, x.remaksseq
+            ROWS UNBOUNDED PRECEDING
+        ) AS balance,
             x.itemnm,
             x.misgubun,
             x.iotype,
@@ -182,24 +202,33 @@ public class CompBalanceDetailServicr {
             x.eumnum,
             x.eumtodt,
             x.tradenm,
-            x.accin,
-            x.accout,
             x.memo,
-            x.remark1,
-            SUM(
-                 COALESCE(x.accin, 0) - COALESCE(x.accout, 0)
-             ) OVER (
-                 PARTITION BY x.id
-                 ORDER BY x.date, x.remaksseq
-                 ROWS UNBOUNDED PRECEDING
-             ) AS balance
+            x.remark1
         FROM union_data x
         ORDER BY x.id, x.date, x.remaksseq
         """;
     List<Map<String, Object>> items = this.sqlRunner.getRows(sql, paramMap);
 //    log.info("거래처별잔액명세서(입금) read SQL: {}", sql);
 //    log.info("SQL Parameters: {}", paramMap.getValues());
+
+  /*  byte[] keyBytes = getKeyFromFile();
+    EncryptionUtil util = new EncryptionUtil();
+
+    for (Map<String, Object> row : items) {
+      Object accnumEnc = row.get("accnum");
+      if (accnumEnc instanceof String encValue && !encValue.isBlank()) {
+        try {
+          String decrypted = util.decrypt(encValue, keyBytes);
+          String masked = decrypted.replaceAll("(?<=^.{4}).", "*");
+          row.put("accnum", masked);
+        } catch (Exception e) {
+          log.warn("계좌번호 복호화 실패", e);
+          row.put("accnum", "****");
+        }
+      }
+    }*/
     return items;
   }
+
 
 }
