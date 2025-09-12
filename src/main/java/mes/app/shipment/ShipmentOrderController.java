a528003d0e4dbe6c4a32f3e74d743c760ad6d6b0
LeeJaeWook <ljw960214@naver.com>
2025-09-12 17:54:14 +0900

    20250912_이재욱_출하등록시 재고데이터 없을시 안내 메세지 출력작업

--- src/main/java/mes/app/shipment/ShipmentOrderController.java
+++ src/main/java/mes/app/shipment/ShipmentOrderController.java
@@ -193,8 +193,12 @@ public class ShipmentOrderController {
 
 			if(summary != null){
 				int totalQty = summary.totalQty; // 출하하려는 재고
-				float currentStock = material.getCurrentStock();
-
+				Float currentStock = material.getCurrentStock();
+				if (currentStock == null) {
+					result.success = false;
+					result.message = "품목 [" + material.getName() + "]의 재고 현황이 존재하지 않습니다.";
+					return result;
+				}
 				if(totalQty > currentStock){
 					result.success = false;
 					result.message = "품목 [" + material.getName() + "]의 출하 수량이 현재고를 초과합니다.";
